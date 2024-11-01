6ebbdffa469f433e3030939924516b2136e7d2a0
EA-Gadgeter <emiacevedoposos@gmail.com>
2024-11-01 10:33:03 -0600

    Creada sidebar con links de la rutas, routerLink, routerLinkOptions con extactPath, indicar pagina activa

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
