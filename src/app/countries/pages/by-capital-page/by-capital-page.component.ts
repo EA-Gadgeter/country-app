67273de03213320143a10a18577d4395aa203d5f
EA-Gadgeter <emiacevedoposos@gmail.com>
2024-11-05 13:58:59 -0600

    Renderizado countries por capital en tabla

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country.interface';
+
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
   styles: ``
 })
 export class ByCapitalPageComponent {
-  public searchByCapital(searchTerm: string) {
+  public countries: Country[] = [];
 
+  constructor(private countriesService: CountriesService) {}
+
+  public SearchByCapital(searchTerm: string) {
+    this.countriesService.SearchByCapital(searchTerm)
+      .subscribe(countries => this.countries = countries)
   }
 }
