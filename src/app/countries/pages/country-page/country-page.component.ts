6cba16aa0a68beeed51a1c55646fee4e0cd69d02
EA-Gadgeter <emiacevedoposos@gmail.com>
2024-11-06 11:55:25 -0600

    Petición de país individual en base a params, evitando el suscribe hell usando observable pipe operators

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { switchMap } from 'rxjs/operators';
+
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService,
+  ) {}
 
+  public ngOnInit(): void {
+    // Podriamos suscribirnos primero a los params, y de ahi nos
+    // volvemos a suscribir para hacer la petición, pero caemos en
+    // algo llamado SUSCRIBE HELL
+    // Podemos usar los el pipe y el operador switchMap, que nos permite
+    // transformar información pero regresando otro observable, y ahi aprovechamos para hacer la petición
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.countriesService.SearchCountryByAlphaCode(id))
+      )
+      .subscribe(country => {
+        if (!country) {
+          this.router.navigateByUrl("");
+        }
+
+        console.log("Tenemos un país")
+      })
+  }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -4,6 +4,8 @@ import { HttpClient } from "@angular/common/http";
 import { Observable, of } from "rxjs";
 import { catchError } from "rxjs/operators";
 
+import { map } from "rxjs/operators";
+
 import type { Country } from "../interfaces/country.interface";
 
 @Injectable({ providedIn: "root" })
@@ -11,6 +13,16 @@ export class CountriesService {
   private API_URL = "https://restcountries.com/v3.1";
 
   constructor(private http: HttpClient) {}
+
+  public SearchCountryByAlphaCode(code: string): Observable<Country | null> {
+    const url = `${this.API_URL}/alpha/${code}`;
+    return this.http.get<Country[]>(url)
+      // Usamos un pipe para regresar null o UN SOLO país
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(() => of(null))
+      );
+  }
 
   public SearchByCapital(term: string): Observable<Country[]> {
     const url = `${this.API_URL}/capital/${term}`;
