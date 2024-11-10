f595a93fc0a4f0303a399aa8825a36bf0b7b81df
EA-Gadgeter <emiacevedoposos@gmail.com>
2024-11-10 10:18:20 -0600

    Cambiado el search-box del search-by-region por las opciones prestablecidas

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -4,6 +4,8 @@ import { CountriesService } from '../../services/countries.service';
 
 import type { Country } from '../../interfaces/country.interface';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,11 +13,16 @@ import type { Country } from '../../interfaces/country.interface';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
+
 
   constructor(private countriesService: CountriesService) {}
 
-  public SearchByRegion(searchTerm: string) {
-    this.countriesService.SearchByRegion(searchTerm)
+  public SearchByRegion(region: Region) {
+    this.selectedRegion = region;
+
+    this.countriesService.SearchByRegion(region)
       .subscribe(countries => this.countries = countries)
   }
 }
